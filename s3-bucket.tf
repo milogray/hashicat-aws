0185826f06debd2260eeea5b8bef46ab9f691666
milogray <oscar.greenwood@gmail.com>
2021-01-14 22:54:01 +0000

    update s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/OscarGreenwood-training/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket_prefix = oscar-greenwood
+  bucket_prefix = oscar_greenwood
 }
